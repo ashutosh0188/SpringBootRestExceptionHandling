f589185cfa0c37eab61f4f8c82993551ad732e42
ashutosh0188 <ashutosh0188@bitbucket.org>
2020-02-27 00:10:47 +0530

    Added basic spring boot rest api with a simple method in rest controller

---
+++ src/main/java/com/example/springbootexceptionhandlingrest/SpringbootexceptionhandlingRestApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootexceptionhandlingrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootexceptionhandlingRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootexceptionhandlingRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springbootexceptionhandlingrest/SpringbootexceptionhandlingRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootexceptionhandlingrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootexceptionhandlingRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
